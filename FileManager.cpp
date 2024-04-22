854778b493695eb767319fd0f5e866d5d6865e7d
kvintes <vnm111195.ru@gmail.com>
2024-04-23 00:18:36 +0700

    add checkFileStates, File-class-declaration

--- FileManager.cpp
+++ FileManager.cpp
@@ -5,7 +5,7 @@ FileManager::FileManager(ILoger* loger, QObject *parent):QObject(parent){
 void FileManager::setLoger(ILoger *loger){
     if(loger){
         this->loger = loger;
-        connect(this, &FileManager::fileChanged, this->loger, &ILoger::outputMessage);
+        connect(this, &FileManager::fileChangedMessage, this->loger, &ILoger::outputMessage);
     }
 }
 void FileManager::addTrackedFile(const QString &filePath){
@@ -24,6 +24,94 @@ bool FileManager::findFileInTrackingFiles(const QString &filePath){
         }
     }
     return false;
+}
+void FileManager::checkFilesStates(){
+    if(this->trackingFiles.size() > 0){
+        QFileInfo fileInfoNow;
+        for(auto file : this->trackingFiles){
+            fileInfoNow.setFile(file.getPath());
+            this->checkFileChanges(fileInfoNow, file);
+        }
+        return;
+    }
+    emit fileChangedMessage(QString("LOG: trackedFiles are absent"));
+}
+void FileManager::checkFileChanges(const QFileInfo& fileNow, FileManager::File& fileOld){
+    QString message = "файл: "+fileOld.getPath();
+    if(fileNow.exists() == fileOld.getExistsStatus()){
+        if(!fileNow.exists() || fileNow.size() == fileOld.getSize()) return;//изменений нет
+        //изменился размер
+        message+=QString(" существует ")
+                +QString(" старый размер: ")+QString::number(fileOld.getSize())
+                +QString(" новый размер: ")+QString::number(fileNow.size());
+        fileOld.setSize(fileNow.size());//сохранили новый размер
+        } else {
+        if(fileNow.exists()){//файл создали
+            message+=QString(" существует ")+QString(" размер: ")+QString::number(fileNow.size());
+            fileOld.setSize(fileNow.size());
+            fileOld.setExistsStatus(fileNow.exists());
+        } else { //файл удалили
+            message+=QString(" НЕ существует ");
+            fileOld.setSize(fileNow.size());
+            fileOld.setExistsStatus(fileNow.exists());
+        }
+    }
+    emit fileChangedMessage(message);
+}
+QString FileManager::getInfo() const{
+    QString info = "begining settings: \n";
+    if(this->trackingFiles.size() > 0){
+        for(int i = 0; i < this->getSize(); ++i){
+            info += " path: " + trackedFiles[i]->getPath();
+            info += "\n status: ";
+            if(trackedFiles[i]->getExistsStatus()){
+                info += " Существует ";
+            } else {
+                info += " НЕ cуществует ";
+            }
+            info += "\n size: " + QString::number(trackedFiles[i]->getSize());
+            info += "\n";
+        }
+    }
+    return info;
+}
+
+
+
+FileManager::File::File(const QString &filePath) {
+    if(filePath.size() != 0){
+        QFileInfo *temp = new QFileInfo(filePath);
+        if(temp){
+            this->filePath = QString(filePath);
+            this->isExists = temp->exists();
+            this->fileSize = temp->size();
+            delete temp;
+        }
+    }
+}
+FileManager::File::File() {
+    this->filePath = QString("");
+    this->isExists = false;
+    this->fileSize = 0;
+}
+QString FileManager::File::getPath() const{
+    return filePath;
+}
+bool FileManager::File::getExistsStatus() const{
+    return isExists;
+}
+int FileManager::File::getSize() const{
+    return fileSize;
+}
+
+QString FileManager::File::setPath(QString path){
+    return filePath = path;
+}
+bool FileManager::File::setExistsStatus(bool status){
+    return isExists = status;
+}
+int FileManager::File::setSize(int size){
+    return fileSize = size;
 }
 
 
