8e8bce2c96e0586fd87e235ff192c9f32a44955f
kvintes <vnm111195.ru@gmail.com>
2024-04-23 09:34:33 +0700

    comment setLogger code, change files

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 
     logger.outputMessage(instance.getInfo());
-    instance.setLoger(&logger);
+    //instance.setLoger(&logger);
 
     QTimer timer(&a);
     timer.start(5000);
